4f79c5795e2ec34784e951af9cfb100f45d122b6
Luca Mattivi <luca@smartdomotik.com>
2016-08-25 18:24:03 +0200

    add mysql envs to /etc/environment

--- cron.sh
+++ cron.sh
@@ -3,6 +3,7 @@
 # set necessary env variables as globals
 set | grep "BACKUP_" > /etc/environment
 set | grep "AWS_" >> /etc/environment
+set | grep "MYSQL_" >> /etc/environment
 
 # change cron schedule
 sed -i "s,CRON_SCHEDULE*,${BACKUP_CRON_SCHEDULE},g" /etc/cron.d/backup-cron
